39151544d2031c9052af89e4539e7fc302b7156c
Neno <tlopezsantos@gmail.com>
2014-11-11 21:33:29 +0100

    Iniciando opengl OK

--- Nenocom/src/org/nenocom/nenocom/OrionActivity.java
+++ Nenocom/src/org/nenocom/nenocom/OrionActivity.java
@@ -1,22 +1,74 @@
 package org.nenocom.nenocom;
 
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 public class OrionActivity extends Activity {
 
+	private GLSurfaceView surface;
+	private boolean rendererEstablecido = false;
+	private MiRenderer renderer = new MiRenderer(this);
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_orion);
+		
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		
+		surface = new GLSurfaceView(this);
+		iniciaOpengl();
+		setContentView(surface);
+		
+		
 	}
-
+	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.orion, menu);
-		return true;
+	public void onPause(){
+		super.onPause();
+		if(rendererEstablecido){
+			surface.onPause();
+		}
 	}
 
+	@Override 
+	protected void onResume() {
+		super.onResume();
+		if (rendererEstablecido) {
+			surface.onResume();
+		}
+	}
+	
+	/**
+	 * Inicia OpenGL ES 2.0 si el dispositivo lo soporta.
+	 */
+	private void iniciaOpengl() {
+		if (soportaOpenGl2()) { 
+			// Establece un contexto compatible con OpenGL ES 2.0. 
+			surface.setEGLContextClientVersion(2);
+			// Asigna el renderer.
+			surface.setRenderer(renderer);
+			rendererEstablecido = true;
+		} else {
+			Toast.makeText(this, "This device does not support OpenGL ES 2.0.", Toast.LENGTH_LONG).show();
+		}
+	}
+
+	/**
+	 * 
+	 * @return true si el dispositivo soporta OpenGL ES 2.0.
+	 */
+	private boolean soportaOpenGl2() {
+		final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+		final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+		return configurationInfo.reqGlEsVersion >= 0x20000;
+	}
 }
