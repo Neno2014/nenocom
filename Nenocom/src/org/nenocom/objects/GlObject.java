b94de5e9ed79513e0ceae330955e98c03b4eb8cc
Neno <tlopezsantos@gmail.com>
2014-11-11 22:44:36 +0100

    GlObject metodos

--- Nenocom/src/org/nenocom/objects/GlObject.java
+++ Nenocom/src/org/nenocom/objects/GlObject.java
@@ -2,4 +2,15 @@ package org.nenocom.objects;
 
 public abstract class GlObject {
 
+	public void onSurfaceCreated() {
+
+	}
+
+	public void onSurfaceChanged(int width, int height) {
+
+	}
+
+	public void onDrawFrame() {
+
+	}
 }
