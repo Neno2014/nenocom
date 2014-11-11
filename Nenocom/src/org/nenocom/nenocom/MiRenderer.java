5c4c0876061f07b1b8f8ef06a4d01e45ad1e17e9
Neno <tlopezsantos@gmail.com>
2014-11-11 21:33:29 +0100

    Iniciando opengl OK

---
+++ Nenocom/src/org/nenocom/nenocom/MiRenderer.java
@@ -0,0 +1,32 @@
+package org.nenocom.nenocom;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView.Renderer;
+
+public class MiRenderer implements Renderer {
+
+	public MiRenderer(OrionActivity orionActivity) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onDrawFrame(GL10 gl) {
+		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		GLES20.glViewport(0, 0, width, height);
+
+	}
+
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		GLES20.glClearColor(0f, 1f, 0f, 1f);
+	}
+
+}
